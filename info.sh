ffb572cd1e5144269836fb719b594bc6ce08787c
gituser <student@gituser.com>
2025-03-23 01:15:56 +0500

    all code has been commited

---
+++ info.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#this is a comment
+echo "Hello World"  #This is also a comment
+
+echo Our shell name is  $BASH
+echo Our shell version name is  $BASH_VERSIOLN
+echo Our home directory is $HOME
+echo Our current working directory is $PWD
+name=Shan
+val=10
+echo The name is $name
+echo The value is $val
